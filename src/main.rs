59ecc58ef4bc4e87f66fc39208d919e240005fa6
Linus Dikomey <l.dikomey03@gmail.com>
2022-12-05 07:52:54 +0100

    day 5

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,14 @@ use std::fs;
 
 mod day3;
 mod day4;
+mod day5;
 
 #[track_caller]
 fn p() -> ! { panic!() }
 
 fn main() {
-    day3::run(&fs::read_to_string("input/day3.txt").unwrap());
-    day4::run(&fs::read_to_string("input/day4.txt").unwrap());
+    //day3::run(&fs::read_to_string("input/day3.txt").unwrap());
+    //day4::run(&fs::read_to_string("input/day4.txt").unwrap());
+    day5::run(&fs::read_to_string("input/day5.txt").unwrap());
+
 }
