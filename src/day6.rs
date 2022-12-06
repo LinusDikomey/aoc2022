5fe4b89e935accc4202eda981b8115ad0f00454f
Linus Dikomey <l.dikomey03@gmail.com>
2022-12-06 13:15:56 +0100

    day 6

--- src/day6.rs
+++ src/day6.rs
@@ -1,8 +1,20 @@
-pub fn run(_input: &str) {
+pub fn run(input: &str) {
     println!("Day 6:");
+    let chars: Vec<_> = input.chars().collect();
     // ---------- part 1 ----------
-    
-    println!("\tPart 1: {}", 0);
+    println!("\tPart 1: {}", find::<4>(&chars));
     // ---------- part 2 ----------
-    println!("\tPart 2: {}", 0);
+    println!("\tPart 2: {}", find::<14>(&chars));
+}
+
+fn find<const N: usize>(chars: &[char]) -> usize {
+    chars
+        .array_windows::<N>()
+        .enumerate()
+        .find(|(_, elems)|
+            elems
+                .iter()
+                .enumerate()
+                .all(|(i, c)| !elems[0..i].contains(c))
+        ).unwrap().0 + N
 }
